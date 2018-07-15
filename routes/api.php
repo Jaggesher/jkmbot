8864c3e5d9742821fc17f85f5b313c6130dca19b
jaggesher <jaggesher14@gmail.com>
2018-07-16 00:38:00 +0600

    Related Route added

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/bot', function(){
+    return response("Fail",401);
+})->middleware('verifybot');
+
+Route::post('/bot','BotController@bot');
